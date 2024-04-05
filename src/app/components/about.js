d9cfc1bc19f53b9a9d9be49e1b3d162c541a9cd9
weechengkhor91 <chengkhor798@gmail.com>
2024-04-05 10:26:32 +0800

    add projects

--- src/app/components/about.js
+++ src/app/components/about.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { Fragment, useState, useTransition } from "react";
 import Image from "next/image";
 import TabButton from "./tab";
@@ -7,96 +7,127 @@ import { PhoneIcon } from "@heroicons/react/24/outline";
 import { EnvelopeIcon } from "@heroicons/react/24/outline";
 
 const TAB_DATA = [
-    {
-        title:"Skills",
-        id:"skills",
-        content:(
-            <ul className="circle pl-4">
-                <li>React JS</li>
-                <li>Next JS</li>
-                <li>Photoshop</li>
-                <li>Figma</li>
-                <li>HTML</li>
-                <li>Bootstrap CSS</li>
-                <li>Javascript</li>
-            </ul>
-        )
-    },
-    {
-        title: "Education",
-        id:"education",
-        content:(
-            <div className="tab-wrapper">
-                <div className="tab-detail">
-                    <div className="tab-header">Universiti Teknikal Malaysia Melaka(UTeM), Malacca - Degree </div>
-                    <div className="tab-year">Sept 2011 - Feb 2015</div>
-                    <div className="tab-title">Bachelor of Computer Science in Interactive Media</div>
-                </div>
-                <div className="tab-detail">
-                    <div className="tab-header">Institut Bukit Mertajam(IBM), Bukit Mertajam-Penang - STPM </div>
-                    <div className="tab-year">Apr 2009 - Dec 2010</div>
-                    <div className="tab-title">Sijil Tinggi Pelajaran Malaysia</div>
-                </div>
-                <div className="tab-detail">
-                    <div className="tab-header">SMK Raja Lope Nor Rashid, Perak - SPM  </div>
-                    <div className="tab-year">Dec 2008</div>
-                    <div className="tab-title">Sijil Pelajaran Malaysia </div>
-                </div>
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="circle pl-4">
+        <li>React JS</li>
+        <li>Next JS</li>
+        <li>Photoshop</li>
+        <li>Figma</li>
+        <li>HTML</li>
+        <li>Bootstrap CSS</li>
+        <li>Javascript</li>
+        <li>Search Engine Optimization (SEO)</li>
+        <li>CMS</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    // tab wrapper
+    content: (
+      <div className="tab-wrapper">
+        <div className="tab-detail">
+          <div className="tab-header">
+            Universiti Teknikal Malaysia Melaka(UTeM), Malacca - Degree{" "}
+          </div>
+          <div className="tab-year">Sept 2011 - Feb 2015</div>
+          <div className="tab-title">
+            Bachelor of Computer Science in Interactive Media
+          </div>
+        </div>
+        <div className="tab-detail">
+          <div className="tab-header">
+            Institut Bukit Mertajam(IBM), Bukit Mertajam-Penang - STPM{" "}
+          </div>
+          <div className="tab-year">Apr 2009 - Dec 2010</div>
+          <div className="tab-title">Sijil Tinggi Pelajaran Malaysia</div>
+        </div>
+        <div className="tab-detail">
+          <div className="tab-header">
+            SMK Raja Lope Nor Rashid, Perak - SPM{" "}
+          </div>
+          <div className="tab-year">Dec 2008</div>
+          <div className="tab-title">Sijil Pelajaran Malaysia </div>
+        </div>
+      </div>
+    ),
+  },
+];
+function About() {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
 
-            </div> // tab wrapper
-        )
-    }
-]
-function About(){
-    const [tab,setTab] = useState("skills");
-    const [isPending, startTransition] = useTransition();
+  const handleChangeTab = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+  return (
+    <section id="sec-about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image
+          src="/images/about.jpg"
+          width={500}
+          height={500}
+          priority
+          alt="About"
+          className="rounded-xl"
+        />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h3 className="text-6xl sm:text-8xl md:text-8xl font-bold text-left text-big-header relative top-5 -z-10">
+            About Me
+          </h3>
+          <h3 className="text-4xl font-bold mb-4 text-left text-my-color">
+            About Me
+          </h3>
+          <div className="flex mb-2">
+            <MapPinIcon className="h-5 w-5" />
+            &nbsp;&nbsp;&nbsp;37, Jalan Rumah Tinggi, #12-435, Singapore 150037.
+          </div>
+          <div className="flex mb-2">
+            <PhoneIcon className="h-5 w-5" />
+            &nbsp;&nbsp;&nbsp;
+            <a
+              href="https://api.whatsapp.com/send?phone=6583998784"
+              target="_blank"
+            >
+              +65 8399 8784
+            </a>{" "}
+            (Whatsapp) / <a href="tel:+601157646683">+6011 5764 6683 </a>
+          </div>
+          <div className="flex mb-2">
+            <EnvelopeIcon className="h-5 w-5" />
+            &nbsp;&nbsp;&nbsp;
+            <a href="mailto:weechengkhor911002@gmail.com" target="_blank">
+              weechengkhor911002@gmail.com
+            </a>
+          </div>
 
-    const handleChangeTab = (id) => {
-        startTransition(()=> {
-            setTab(id);
-        })
-    }
-     return (
-        <section id="sec-about">
-            <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-                <Image src="/images/about.jpg" width={500} height={500} priority alt="About" className="rounded-xl"/>
-                <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-                <h3 className="text-6xl sm:text-8xl md:text-8xl font-bold text-left text-big-header relative top-5 -z-10">About Me</h3>
-                <h3 className="text-4xl font-bold mb-4 text-left text-my-color">About Me</h3>
-                <div className="flex mb-2">
-                <MapPinIcon className="h-5 w-5"/>&nbsp;&nbsp;&nbsp;37, Jalan Rumah Tinggi, #12-435, Singapore 150037.
-                </div>
-                <div className="flex mb-2">
-                <PhoneIcon className="h-5 w-5"/>&nbsp;&nbsp;&nbsp;<a href="https://api.whatsapp.com/send?phone=6583998784" target="_blank">+65 8399 8784</a> (Whatsapp) / <a href="tel:+601157646683">+6011 5764 6683 </a>
-                </div>
-                <div className="flex mb-2">
-                <EnvelopeIcon className="h-5 w-5"/>&nbsp;&nbsp;&nbsp;<a href="mailto:weechengkhor911002@gmail.com" target="_blank">weechengkhor911002@gmail.com</a>
-                </div>
-                
-        
-
-
-
-
-                    <div className="flex flex-row mt-8">
-                        <TabButton selectTab={()=> handleChangeTab("skills")} active={tab=== "skills"}>
-                            Skills
-                        </TabButton>
-                        <TabButton selectTab={()=> handleChangeTab("education")} active={tab=== "education"}>
-                            Education
-                        </TabButton>
-
-                    </div>
-                    <div className="mt-8">
-                        {
-                            TAB_DATA.find((item) => item.id === tab).content
-                        }
-                    </div>
-                </div>
-            </div>
-
-        </section>
-    )
+          <div className="flex flex-row mt-8">
+            <TabButton
+              selectTab={() => handleChangeTab("skills")}
+              active={tab === "skills"}
+            >
+              Skills
+            </TabButton>
+            <TabButton
+              selectTab={() => handleChangeTab("education")}
+              active={tab === "education"}
+            >
+              Education
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((item) => item.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,63 +1,72 @@
-'use client';
+"use client";
 import { Fragment } from "react";
 import Link from "next/link";
 import NavLink from "./navlink";
 import { useState } from "react";
-import { Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./menuoverlay";
 import Image from "next/image";
 
 const navLinks = [
-    {
-        title: "About",
-        path:"#sec-about"
-    },
-    {
-        title:"Projects",
-        path:"#sec-projects"
-    },
-    {
-        title:"Contact",
-        path:"#sec-contact"
-    }
-]
-function Navbar(){
-    const [navbar, setNavbar] = useState(false);
-   function handleClickClose(val){
-        console.log("val",val);
-        setNavbar(val);
-    }
-    return <Fragment>
-        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#fff] shadow ">
-            <div className="flex flex-wrap items-center justify-between mx-auto py-4 px-3">
-                <Link href="/" className="text-4xl font-bold">Portfolio</Link>
-                <div className="mobile-menu block md:hidden">
-                    {
-                        !navbar ? 
-                        <button onClick={() => setNavbar(true)} className="flex items-center px-3 py-2 border rounded">
-                            <Bars3Icon className="h-5 w-5"/>
-                        </button> 
-                        : 
-                        <button onClick={() => setNavbar(false)} className="flex items-center px-3 py-2 border rounded">
-                        <XMarkIcon className="h-5 w-5"/>
-                    </button> 
-                    }
-                </div>
-                <div className="menu hidden md:block md:w-auto" id="navbar">
-                    <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8">
-                        {
-                            navLinks.map((item,index)=> 
-                            <li key={index}>
-                                <NavLink href={item.path} title={item.title} />
-                            </li> )
-                        }
-                        
-                    </ul>
-                </div>
-            </div>
-            { navbar && <MenuOverlay links={navLinks} onClickClose={handleClickClose}/> }
-        </nav>
+  {
+    title: "About",
+    path: "#sec-about",
+  },
+  {
+    title: "Projects",
+    path: "#sec-projects",
+  },
+  {
+    title: "Contact",
+    path: "#sec-contact",
+  },
+];
+function Navbar() {
+  const [navbar, setNavbar] = useState(false);
+  function handleClickClose(val) {
+    console.log("val", val);
+    setNavbar(val);
+  }
+  return (
+    <Fragment>
+      <nav className="fixed top-0 left-0 right-0 z-10 bg-[#fff] shadow ">
+        <div className="flex flex-wrap items-center justify-between mx-auto py-4 px-3">
+          <Link href="/" className="text-4xl font-bold">
+            Portfolio
+          </Link>
+          <div className="mobile-menu block md:hidden">
+            {!navbar ? (
+              <button
+                onClick={() => setNavbar(true)}
+                className="flex items-center px-3 py-2"
+              >
+                <Bars3Icon className="h-6 w-6" />
+              </button>
+            ) : (
+              <button
+                onClick={() => setNavbar(false)}
+                className="flex items-center px-3 py-2 "
+              >
+                <XMarkIcon className="h-6 w-6" />
+              </button>
+            )}
+          </div>
+          <div className="menu hidden md:block md:w-auto" id="navbar">
+            <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8">
+              {navLinks.map((item, index) => (
+                <li key={index}>
+                  <NavLink href={item.path} title={item.title} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        {navbar && (
+          <MenuOverlay links={navLinks} onClickClose={handleClickClose} />
+        )}
+      </nav>
     </Fragment>
+  );
 }
 
 export default Navbar;
